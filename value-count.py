3a966b744f91fa3585d7443f49bc50d1d41e95d1
Christian Serrano <nano561@icloud.com>
2019-07-31 22:54:49 -0400

    Updated value-count.py
    Updated: logic to see if there actual bills in the drawer

--- value-count.py
+++ value-count.py
@@ -1,20 +1,18 @@
 # Provide the value you want to use (US currency [common])
 valueList = [100, 50, 20, 10, 5, 1, .25, .10, .05, .01]
 # Provide the count of each value
-countOfValue = {100: 10, 50: 9, 20: 8, 10: 7, 5: 6, 1: 5, .25: 4, .10: 3, .05: 2, .01: 1}
+countOfValue = {100: 10, 50: 10, 20: 10, 10: 10, 5: 10, 1: 10, .25: 10, .10: 1, .05: 10, .01: 10}
 # Provide the change need to return
 changeNeeded = 105.23
 # The list that will give you the change you need to give
 changeBack = []
 
 for i in valueList:
-    print(i)
-    multiplier = changeNeeded / i
-    Continue = changeNeeded % i
+    changeCount = changeNeeded / i
 
-    if multiplier >= 1:
-        changeBack.append((i, int(multiplier)))
-        changeNeeded = changeNeeded - (int(multiplier) * i)
+    if changeCount >= 1 and countOfValue[i] >= int(changeCount):
+        changeBack.append((i, int(changeCount)))
+        changeNeeded = changeNeeded - (i * int(changeCount))
 
 print(changeBack)
 
