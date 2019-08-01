2ba7b7bad25d32cc5f0b0b4977b8f4fc43520984
Christian Serrano <nano561@icloud.com>
2019-07-31 23:14:46 -0400

    Updated value-count.py
    Updated: logic to give you the lowest count of change

--- value-count.py
+++ value-count.py
@@ -1,9 +1,9 @@
 # Provide the value you want to use (US currency [common])
 valueList = [100, 50, 20, 10, 5, 1, .25, .10, .05, .01]
-# Provide the count of each value
+# Provide the count of each value (highest to lowest)
 countOfValue = {100: 10, 50: 10, 20: 10, 10: 10, 5: 10, 1: 10, .25: 10, .10: 1, .05: 10, .01: 10}
 # Provide the change need to return
-changeNeeded = 105.23
+changeNeeded = 205.23
 # The list that will give you the change you need to give
 changeBack = []
 
@@ -13,6 +13,9 @@ for i in valueList:
     if changeCount >= 1 and countOfValue[i] >= int(changeCount):
         changeBack.append((i, int(changeCount)))
         changeNeeded = changeNeeded - (i * int(changeCount))
+    elif changeCount >= 1 and countOfValue[i] > 0:
+        changeBack.append((i, countOfValue[i]))
+        changeNeeded = changeNeeded - (i * countOfValue[i])
 
 print(changeBack)
 
