80b264ad907c2324ae2df1956c3f31596cb4f8ef
Christian Serrano <nano561@icloud.com>
2019-08-01 11:25:38 -0400

    Created module i_need_change
    This will allow you to call the function whenever you need to, you need to provide a dict of what values you have in the drawer and the count of each value and pass also the change you need. It will what is left in the draw, what you need to give back for each value and if you still owe them money
    
    There is a bug still in play when it comes to decimal points, sometime it will short you a couple of penny's most of the time just a single penny.

--- value-count.py
+++ value-count.py
@@ -1,21 +1,33 @@
 # Provide the count of each value (highest to lowest)
-countOfValue = {100: 1, 50: 10, 20: 10, 10: 10, 5: 10, 1: 10, .25: 10, .10: 1, .05: 10, .01: 10}
+countOfValue = {100: 2, 50: 10, 20: 10, 10: 10, 5: 10, 1: 10, .25: 10, .10: 1, .05: 10, .01: 10}
 # Provide the change need to return
-changeNeeded = 205.23
-# The list that will give you the change you need to give
-changeBack = []
+changeNeeded = 200.98
 
-for i in countOfValue.keys():
-    changeCount = changeNeeded / i
 
-    if changeCount >= 1 and countOfValue[i] >= int(changeCount):
-        changeBack.append((i, int(changeCount)))
-        changeNeeded = changeNeeded - (i * int(changeCount))
-        countOfValue[i] = countOfValue[i] - int(changeCount)
-    elif changeCount >= 1 and countOfValue[i] > 0:
-        changeBack.append((i, countOfValue[i]))
-        changeNeeded = changeNeeded - (i * countOfValue[i])
-        countOfValue[i] = 0
+# The list that will give you the change you need, what is left in the register and if there is not enough money in
+# the draw to give you the total that is missing
+def i_need_change(count_of_value, change_needed):
+    change_back = []
+    for i in count_of_value.keys():
+        changeCount = change_needed / i
 
-print(changeBack)
-print(countOfValue)
+        if changeCount >= 1 and count_of_value[i] >= int(changeCount):
+            change_back.append((i, int(changeCount)))
+            change_needed = change_needed - (i * int(changeCount))
+            count_of_value[i] = count_of_value[i] - int(changeCount)
+        elif changeCount >= 1 and count_of_value[i] > 0:
+            change_back.append((i, count_of_value[i]))
+            change_needed = change_needed - (i * count_of_value[i])
+            count_of_value[i] = 0
+
+    if 0.009 <= change_needed <= 0.01:
+        change_needed = 0.00
+    return {"count_of_value": count_of_value, "change_back": change_back, "change_needed": change_needed}
+
+
+x = i_need_change(countOfValue, changeNeeded)
+print(x["count_of_value"])
+print(x["change_back"])
+print(x["change_needed"])
+
+print(0.1/0.1)
